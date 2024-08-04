bb6d24083dbfdc58ea77c9aa4db5a25e160894c6
vishnu210603 <138102445+vishnu210603@users.noreply.github.com>
2024-08-05 03:41:03 +0530

    Update bg.jsx

--- src/components/bg.jsx
+++ src/components/bg.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Bg = () => {
   return (
     <div className='bg-college bg-repeat-x p-0 h-fit'>
-     <div className=" w-[20%] h-[450px]  py-36 ">
+     <div className=" w-[50%] sm:h-[530px] h-fit  py-36 ">
         <img
-        src="https://static.vecteezy.com/system/resources/previews/022/483/489/original/respectful-3d-student-boy-with-good-manners-on-white-background-transparent-background-free-png.png" className="h-contain"
+        src="https://static.vecteezy.com/system/resources/previews/022/483/489/original/respectful-3d-student-boy-with-good-manners-on-white-background-transparent-background-free-png.png" className="sm:h-[530px] h-fit"
         />
       </div> 
     </div>
