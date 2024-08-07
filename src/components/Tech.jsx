6638665fe69ce1965b6875c64b40a05d5c9b170e
vishnu210603 <138102445+vishnu210603@users.noreply.github.com>
2024-08-07 10:56:06 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -17,32 +17,52 @@
 
 // export default Tech
 
-import React from 'react';
-import { styles } from '../styles';
-import { fadeIn, textVariant } from '../utils/motion'
-import BallCanvas from './canvas/Ball'; // Ensure the correct import path
-import { SectionWrapper } from '../hoc'; // Ensure this is correctly defined
-import { technologies } from '../constants'; // Ensure this is correctly defined
-import { motion } from 'framer-motion';
+// import React from 'react';
+// import { styles } from '../styles';
+// import { fadeIn, textVariant } from '../utils/motion'
+// import BallCanvas from './canvas/Ball'; // Ensure the correct import path
+// import { SectionWrapper } from '../hoc'; // Ensure this is correctly defined
+// import { technologies } from '../constants'; // Ensure this is correctly defined
+// import { motion } from 'framer-motion';
 
+
+// const Tech = () => {
+//   return (
+//     <motion.div variants={textVariant()}>
+//      <p className={styles.sectionSubText}>| SKILLS</p>
+//      <h2 className={styles.heroHeadText}>Technologies.  </h2>
+    
+//     <div className='flex flex-row flex-wrap justify-center gap-10'>
+      
+//       <div className=''></div>
+//       {technologies.map((technology) => (
+//         <div className='w-28 h-28 mt-10' key={technology.name}>
+//           <BallCanvas icon={technology.icon} />
+//         </div>
+//       ))}
+//     </div>
+//     </motion.div>
+//   );
+// };
+
+// export default SectionWrapper(Tech, 'tech'); // Ensure SectionWrapper is correctly used
+
+import React from "react";
+
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <motion.div variants={textVariant()}>
-     <p className={styles.sectionSubText}>| SKILLS</p>
-     <h2 className={styles.heroHeadText}>Technologies.  </h2>
-    
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-      
-      <div className=''></div>
       {technologies.map((technology) => (
-        <div className='w-28 h-28 mt-10' key={technology.name}>
+        <div className='w-28 h-28' key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
       ))}
     </div>
-    </motion.div>
   );
 };
 
-export default SectionWrapper(Tech, 'tech'); // Ensure SectionWrapper is correctly used
+export default SectionWrapper(Tech, "");
