d00eab5ef2540139dbc7f12f8237240ad9ea4ba3
vishnu2106 <vishnuyadav2106@gmail.com>
2024-08-13 11:29:09 +0530

    tech updated

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,51 +1,3 @@
-// import React, { Suspense } from 'react';
-// import { Canvas } from '@react-three/fiber';
-// import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
-// import CanvasLoader from '../Loader';
-
-// const Ball = (props) => {
-//   const [decal] = useTexture([props.imgUrl]);
-
-//   return (
-//     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-//       <ambientLight intensity={0.25} />
-//       <directionalLight position={[1, 1, 0.1]} />
-
-//       <mesh castShadow receiveShadow scale={2.75}>
-//         <icosahedronGeometry args={[1, 1]} />
-//         <meshStandardMaterial
-//           color="#ba55d3"
-//           shadow= "#fff"
-//           polygonOffset
-//           polygonOffsetFactor={-5}
-//           flatShading
-//         />
-//         <Decal
-//           map={decal}
-//           position={[0, 0, 1]}
-//           rotation={[2 * Math.PI, 0, 6.25]}
-//           flatShading
-//         />
-//       </mesh>
-//     </Float>
-//   );
-// };
-
-// const BallCanvas = ({ icon }) => {
-//   return (
-//     <Canvas frameloop='demand' gl={{ preserveDrawingBuffer: true }}>
-//       <Suspense fallback={<CanvasLoader />}></Suspense>
-//         <OrbitControls enableZoom={false} />
-//         <Ball imgUrl={icon} />
-     
-//       <Preload all />
-//     </Canvas>
-//   );
-// };
-
-// export default BallCanvas;
-
-"use strict"
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
@@ -63,6 +15,7 @@ const Ball = (props) => {
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
           color="#ba55d3"
+          shadow= "#fff"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
@@ -79,27 +32,74 @@ const Ball = (props) => {
 };
 
 const BallCanvas = ({ icon }) => {
-  const handleContextLost = (event) => {
-    event.preventDefault();
-    // Logic to handle context loss
-  };
-
   return (
-    <Canvas
-      frameloop='demand'
-      gl={{ preserveDrawingBuffer: true, antialias: true }}
-      pixelRatio={Math.min(window.devicePixelRatio, 2)}
-      onCreated={({ gl }) => {
-        gl.domElement.addEventListener('webglcontextlost', handleContextLost, false);
-      }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
+    <Canvas frameloop='demand' gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}></Suspense>
         <OrbitControls enableZoom={false} />
         <Ball imgUrl={icon} />
-      </Suspense>
+     
       <Preload all />
     </Canvas>
   );
 };
 
 export default BallCanvas;
+
+// "use strict"
+// import React, { Suspense } from 'react';
+// import { Canvas } from '@react-three/fiber';
+// import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
+// import CanvasLoader from '../Loader';
+
+// const Ball = (props) => {
+//   const [decal] = useTexture([props.imgUrl]);
+
+//   return (
+//     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+//       <ambientLight intensity={0.25} />
+//       <directionalLight position={[1, 1, 0.1]} />
+
+//       <mesh castShadow receiveShadow scale={2.75}>
+//         <icosahedronGeometry args={[1, 1]} />
+//         <meshStandardMaterial
+//           color="#ba55d3"
+//           polygonOffset
+//           polygonOffsetFactor={-5}
+//           flatShading
+//         />
+//         <Decal
+//           map={decal}
+//           position={[0, 0, 1]}
+//           rotation={[2 * Math.PI, 0, 6.25]}
+//           flatShading
+//         />
+//       </mesh>
+//     </Float>
+//   );
+// };
+
+// const BallCanvas = ({ icon }) => {
+//   const handleContextLost = (event) => {
+//     event.preventDefault();
+//     // Logic to handle context loss
+//   };
+
+//   return (
+//     <Canvas
+//       frameloop='demand'
+//       gl={{ preserveDrawingBuffer: true, antialias: true }}
+//       pixelRatio={Math.min(window.devicePixelRatio, 2)}
+//       onCreated={({ gl }) => {
+//         gl.domElement.addEventListener('webglcontextlost', handleContextLost, false);
+//       }}
+//     >
+//       <Suspense fallback={<CanvasLoader />}>
+//         <OrbitControls enableZoom={false} />
+//         <Ball imgUrl={icon} />
+//       </Suspense>
+//       <Preload all />
+//     </Canvas>
+//   );
+// };
+
+// export default BallCanvas;
